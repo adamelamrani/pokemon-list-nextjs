b1e4cef078de574ad75a8e988f4feaf260664a45
Adam El Amrani <adelamco1091@outlook.com>
2022-03-01 17:11:09 +0100

    Created navigation, home and layout

---
+++ pages/home.js
@@ -0,0 +1,33 @@
+import Layout from "./Layout";
+
+const home = () => {
+  return (
+    <>
+      <Layout>
+        <h1>Low-cost sad-looking Pokémon page</h1>
+        <p>
+          Pokémon[a] is a series of video games developed by Game Freak and
+          published by Nintendo and The Pokémon Company under the Pokémon media
+          franchise. Created by Satoshi Tajiri with assistance from Ken
+          Sugimori, the first games, Pocket Monsters Red and Green, released in
+          1996 in Japan for the Game Boy, later released outside of Japan as
+          Pokémon Red and Blue. The main series of role-playing video games
+          (RPGs), referred as the "core series" by their developers,[1][2][3]
+          have continued on each generation of Nintendo's handhelds. The most
+          recently released core series game, Pokémon Legends: Arceus, was
+          released on January 28, 2022, for the Nintendo Switch. It is a prequel
+          to the 2006 Nintendo DS games Pokémon Diamond and Pearl. The core
+          games are released in generations, each with different Pokémon,
+          storylines, and characters. Remakes of the games are usually released
+          around a decade after the original versions for the latest console at
+          the time. While the main series consists of RPGs developed by Game
+          Freak, many spin-off games based on the series have been developed by
+          various companies, encompassing other genres such as action
+          role-playing, puzzle, fighting, and digital pet games..
+        </p>
+      </Layout>
+    </>
+  );
+};
+
+export default home;
